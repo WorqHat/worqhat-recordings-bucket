703625df451812304c18514b2b23e396c6ddc2f8
Sagnik Ghosh <sagnik@worqhat.com>
2022-08-22 00:22:20 +0530

    Update changes

--- README.md
+++ README.md
@@ -11,8 +11,8 @@ a custom script that will configure your S3 bucket for WorqHat Call recordings.
 
 ## Instructions
 
-- `npx cdk bootstrap --context Subdomain=worqhat --context s3bucketName=[bucket_name] --context  s3bucketRegion=[bucket_region]`
-- `npx cdk deploy --context Subdomain=worqhat --context s3bucketName=[bucket_name] --context s3bucketRegion=[bucket_region]`
+- `npx cdk bootstrap --context worqhatSubdomain=[worqhat_subdomain] --context s3bucketName=[bucket_name] --context  s3bucketRegion=[bucket_region]`
+- `npx cdk deploy --context worqhatSubdomain=[worqhat_subdomain] --context s3bucketName=[bucket_name] --context s3bucketRegion=[bucket_region]`
 
 The output of the `cdk deploy` command will include the names of the
 s3 bucket and the IAM role configured for WorqHat.
--- bin/worqhat-recordings-bucket.ts
+++ bin/worqhat-recordings-bucket.ts
@@ -1,60 +1,60 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { WorqhatRecordingBucket } from "../lib/worqhat-recordings-bucket-stack";
+import { worqhatRecordingBucket } from "../lib/worqhat-recordings-bucket-stack";
 
 const app = new cdk.App();
 
-const subdomain = app.node.tryGetContext("Subdomain");
+const subdomain = app.node.tryGetContext("worqhatSubdomain");
 if (
-  subdomain === undefined ||
-  !(typeof subdomain === "string") ||
-  subdomain.trim() === ""
+    subdomain === undefined ||
+    !(typeof subdomain === "string") ||
+    subdomain.trim() === ""
 ) {
   throw new Error(
-    "Must pass a '-c Subdomain=<worqhat>' context parameter"
+      "Must pass a '-c worqhatSubdomain=<worqhat_subdomain>' context parameter"
   );
 }
 
 const s3bucketName = app.node.tryGetContext("s3bucketName");
 if (
-  s3bucketName === undefined ||
-  !(typeof s3bucketName === "string") ||
-  s3bucketName.trim() === ""
+    s3bucketName === undefined ||
+    !(typeof s3bucketName === "string") ||
+    s3bucketName.trim() === ""
 ) {
   throw new Error(
-    "Must pass a '-c Subdomain=<worqhat> s3bucketName=<bucket_name> s3Region=<bucket region>' context parameter"
+      "Must pass a '-c worqhatSubdomain=<worqhat_subdomain> s3bucketName=<bucket_name> s3Region=<bucket region>' context parameter"
   );
 }
 
 const s3bucketRegion = app.node.tryGetContext("s3bucketRegion");
 if (
-  s3bucketRegion === undefined ||
-  !(typeof s3bucketRegion === "string") ||
-  s3bucketRegion.trim() === ""
+    s3bucketRegion === undefined ||
+    !(typeof s3bucketRegion === "string") ||
+    s3bucketRegion.trim() === ""
 ) {
   throw new Error(
-    "Must pass a '-c Subdomain=<worqhat> s3bucketName=<bucket_name> s3Region=<bucket region>' context parameter"
+      "Must pass a '-c worqhatSubdomain=<worqhat_subdomain> s3bucketName=<bucket_name> s3Region=<bucket region>' context parameter"
   );
 }
 
-new WorqhatRecordingBucket(
-  app,
-  `worqhatRecordingBucket-${subdomain}-${s3bucketName}-${s3bucketRegion}`,
-  {
-    /* If you don't specify 'env', this stack will be environment-agnostic.
-     * Account/Region-dependent features and context lookups will not work,
-     * but a single synthesized template can be deployed anywhere. */
+new worqhatRecordingBucket(
+    app,
+    `worqhatRecordingBucket-${subdomain}-${s3bucketName}-${s3bucketRegion}`,
+    {
+      /* If you don't specify 'env', this stack will be environment-agnostic.
+       * Account/Region-dependent features and context lookups will not work,
+       * but a single synthesized template can be deployed anywhere. */
 
-    /* Uncomment the next line to specialize this stack for the AWS Account
-     * and Region that are implied by the current CLI configuration. */
-    // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+      /* Uncomment the next line to specialize this stack for the AWS Account
+       * and Region that are implied by the current CLI configuration. */
+      // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
-    /* Uncomment the next line if you know exactly what Account and Region you
-     * want to deploy the stack to. */
-    // env: { account: '123456789012', region: 'us-east-1' },
+      /* Uncomment the next line if you know exactly what Account and Region you
+       * want to deploy the stack to. */
+      // env: { account: '123456789012', region: 'us-east-1' },
 
-    /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-    env: { region: s3bucketRegion },
-  }
+      /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+      env: { region: s3bucketRegion },
+    }
 );
